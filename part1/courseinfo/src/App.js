ff31c6d209c39b057d723b5d6556b10b5c5fdb22
Pavel Kurach <pavel.kurach1@gmail.com>
2023-04-05 08:17:34 +0200

    Finish exercise 1.1

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,3 +1,30 @@
+const Header = (props) => {
+	return <h1>{props.course}</h1>;
+};
+
+const Content = (props) => {
+	const content = props.exercises.map((obj) => {
+		return (
+			<p>
+				{obj.part} {obj.exercises}
+			</p>
+		);
+	});
+	return <>{content}</>;
+};
+
+const Total = (props) => {
+	return (
+		<p>
+			Number of exercises{" "}
+			{props.exercises.reduce(
+				(previousValue, currentValue) => previousValue + currentValue,
+				0,
+			)}
+		</p>
+	);
+};
+
 const App = () => {
 	const course = "Half Stack application development";
 	const part1 = "Fundamentals of React";
@@ -9,17 +36,15 @@ const App = () => {
 
 	return (
 		<div>
-			<h1>{course}</h1>
-			<p>
-				{part1} {exercises1}
-			</p>
-			<p>
-				{part2} {exercises2}
-			</p>
-			<p>
-				{part3} {exercises3}
-			</p>
-			<p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+			<Header course={course} />
+			<Content
+				exercises={[
+					{ part: part1, exercises: exercises1 },
+					{ part: part2, exercises: exercises2 },
+					{ part: part3, exercises: exercises3 },
+				]}
+			/>
+			<Total exercises={[exercises1, exercises2, exercises3]} />
 		</div>
 	);
 };
