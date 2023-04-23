90ce252751e806d237fed7837f8ca0eba57fec25
Pavel Kurach <pavel.kurach1@gmail.com>
2023-04-23 16:52:38 +0200

    Complete exercises 3.15 — 3.18

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,88 +1,129 @@
+/* eslint-disable no-console */
+/* eslint-disable prefer-destructuring */
 const express = require("express");
-const { v4: uuid } = require("uuid");
 const morgan = require("morgan");
 const cors = require("cors");
 const Person = require("./models/person");
 
 const app = express();
 
-morgan.token("body", req => {
-  return JSON.stringify(req.body);
-});
+morgan.token("body", req => JSON.stringify(req.body));
 
+app.use(express.static("build"));
 app.use(express.json());
 app.use(morgan(":method :url :status :body - :response-time ms"));
 app.use(cors());
-app.use(express.static("build"));
 
-app.get("/info", (request, response) => {
-  response.send(
-    `<div>
-        Phonebook has info of ${persons.length} people.   
+app.get("/api/persons/", (request, response, next) => {
+  Person.find({})
+    .then(persons => {
+      response.json(persons);
+    })
+    .catch(error => next(error));
+});
+
+app.get("/info", (request, response, next) => {
+  Person.countDocuments()
+    .then(numberOfPersons => {
+      response.send(
+        `<div>
+        Phonebook has info of ${numberOfPersons} people.   
         ${new Date()}
     </div>`,
-  );
+      );
+    })
+    .catch(error => next(error));
 });
 
-app.get("/api/persons", (request, response) => {
-  Person.find({}).then(persons => {
-    response.json(persons);
-  });
-});
-
-app.get("/api/persons/:id", (request, response) => {
+app.get("/api/persons/:id", (request, response, next) => {
   const id = request.params.id;
   Person.findById(id)
     .then(person => {
-      response.json(person);
+      if (person) {
+        response.json(person);
+      } else {
+        response.status(404);
+        response.json({ error: `no user with id ${id}` });
+      }
     })
-    .catch(error => {
-      response.status(404);
-      response.json({ error: `no user with id ${id}` });
-    });
+    .catch(error => next(error));
 });
 
-app.delete("/api/persons/:id", (request, response) => {
-  const id = request.params.id;
-  persons = persons.filter(person => person.id != id);
-  response.status(204).end();
+app.delete("/api/persons/:id", (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then(() => {
+      response.status(204).end();
+    })
+    .catch(error => next(error));
 });
 
-app.post("/api/persons/", (request, response) => {
-  const _name = request.body?.name;
-  const _number = request.body?.number;
+app.post("/api/persons/", (request, response, next) => {
+  const newPersonName = request.body?.name;
+  const newPersonNumber = request.body?.number;
 
-  if (!_number) {
+  if (!newPersonNumber) {
     response.status(400);
     response.json({ error: "number is missing" });
   }
 
-  if (!_name) {
+  if (!newPersonName) {
     response.status(400);
     response.json({ error: "name is missing" });
   } else {
-    Person.findOne({ name: _name })
+    Person.findOne({ name: newPersonName })
       .then(person => {
         if (person) {
           response.status(400);
           response.json({ error: "name must be unique" });
         } else {
-          newPerson = new Person({
-            name: _name,
-            number: _number,
+          const newPerson = new Person({
+            name: newPersonName,
+            number: newPersonNumber,
           });
-          newPerson.save().then(person => {
+          newPerson.save().then(createdPerson => {
             response.status(201);
-            response.json(person);
+            response.json(createdPerson);
           });
         }
       })
-      .catch(error => {
-        response.status(404);
-        response.json({ error: String(error) });
-      });
+      .catch(error => next(error));
   }
 });
+
+app.put("/api/persons/:id", (request, response, next) => {
+  const personNumber = request.body?.number;
+
+  if (!personNumber) {
+    response.status(400);
+    response.json({ error: "number is missing" });
+  }
+
+  const person = {
+    number: personNumber,
+  };
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedNote => {
+      response.json(updatedNote);
+    })
+    .catch(error => next(error));
+});
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+app.use(unknownEndpoint);
+
+// eslint-disable-next-line no-unused-vars
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+
+  return response.status(500).end();
+};
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
