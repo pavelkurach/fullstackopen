11d76f27d2bfcec78df2f8bff2604697f99f806a
Pavel Kurach <pavel.kurach1@gmail.com>
2023-04-24 20:45:57 +0200

    Complete exercise 4.1

---
+++ part4/bloglist/models/blog.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+const url = require('../utils/config').MONGODB_URI;
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
+});
+
+const Blog = mongoose.model('Blog', blogSchema);
+
+mongoose
+  .connect(url)
+  .then(() => {
+    console.log('connected to MongoDB');
+  })
+  .catch(error => {
+    console.log('error connecting to MongoDB:', error.message);
+  });
+
+module.exports = Blog;
