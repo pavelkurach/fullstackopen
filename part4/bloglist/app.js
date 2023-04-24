9176124c749a1f88a1658bff98d0e169f8122f87
Pavel Kurach <pavel.kurach1@gmail.com>
2023-04-24 20:53:13 +0200

    Complete exercise 4.2

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -1,12 +1,22 @@
 const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+
+const unknownEndpoint = require('./utils/middleware').unknownEndpoint;
+const errorHandler = require('./utils/middleware').errorHandler;
+const blogsRouter = require('./controllers/blogs');
 
 const app = express();
-const cors = require('cors');
-const blogsRouter = require('./controllers/blogs');
 
 app.use(cors());
 app.use(express.json());
 
+morgan.token('body', req => JSON.stringify(req.body));
+app.use(morgan(':method :url :status :body - :response-time ms'));
+
 app.use('/api/blogs', blogsRouter);
+
+app.use(unknownEndpoint);
+app.use(errorHandler);
 
 module.exports = app;
---
+++ part4/bloglist/utils/middleware.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-unused-vars */
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' });
+};
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' });
+  }
+  if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message });
+  }
+
+  return response.status(500).end();
+};
+
+module.exports = {
+  unknownEndpoint,
+  errorHandler,
+};
