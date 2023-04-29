b1d9d84cf10f9cf41d794e425f0bb16adefd05c9
Pavel Kurach <pavel.kurach1@gmail.com>
2023-04-29 12:32:24 +0200

    Complete exercises 4.3, 4.4

--- part4/bloglist/tests/list_helper.test.js
+++ part4/bloglist/tests/list_helper.test.js
@@ -43,3 +43,55 @@ describe('total likes', () => {
     expect(result).toBe(22);
   });
 });
+
+describe('favorite blog', () => {
+  const listOfBlogs = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0,
+    },
+    {
+      _id: '5a422a851b54a676234d17f7',
+      title: 'React patterns',
+      author: 'Michael Chan',
+      url: 'https://reactpatterns.com/',
+      likes: 7,
+      __v: 0,
+    },
+    {
+      _id: '5a422b891b54a676234d17fa',
+      title: 'First class tests',
+      author: 'Robert C. Martin',
+      url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
+      likes: 10,
+      __v: 0,
+    },
+  ];
+
+  test('when list has no blogs blog, returns empty object', () => {
+    const result = listHelper.favoriteBlog([]);
+    expect(result).toEqual({});
+  });
+
+  test('when list has only one blog, returns its title, author and likes', () => {
+    const result = listHelper.favoriteBlog([listOfBlogs[0]]);
+    expect(result).toEqual({
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      likes: 5,
+    });
+  });
+
+  test('when list has multiple blogs, returns the one with most likes', () => {
+    const result = listHelper.favoriteBlog(listOfBlogs);
+    expect(result).toEqual({
+      title: 'First class tests',
+      author: 'Robert C. Martin',
+      likes: 10,
+    });
+  });
+});
--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,6 +1,12 @@
 const totalLikes = blogs =>
   blogs.map(blog => blog.likes).reduce((acc, curr) => acc + curr, 0);
 
+const favoriteBlog = blogs =>
+  (({ title, author, likes }) => ({ title, author, likes }))(
+    blogs.reduce((fav, curr) => (curr ? curr.likes > fav.likes : fav)),
+  );
+
 module.exports = {
   totalLikes,
+  favoriteBlog,
 };
