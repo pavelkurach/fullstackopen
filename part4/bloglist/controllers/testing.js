c21f99886676d8ca05ea011fdf17d8d6a87b382e
Pavel Kurach <pavel.kurach1@gmail.com>
2023-05-19 21:45:47 +0200

    Add testing router

--- part4/bloglist/controllers/testing.js
+++ part4/bloglist/controllers/testing.js
@@ -2,6 +2,6 @@ import Blog from '../models/blog';
 
 const testingRouter = require('express').Router();
 
-testingRouter.post('/reset', async () => {
+testingRouter.post('/reset/', async () => {
   await Blog.deleteMany({});
 });
