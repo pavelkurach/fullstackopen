5f117d1cdf24e6264ba6e0ecb62270da715f63a0
Pavel Kurach <pavel.kurach1@gmail.com>
2023-05-19 12:15:56 +0200

    Complete exercise 5.10

--- part5/src/App.js
+++ part5/src/App.js
@@ -107,14 +107,16 @@ const App = () => {
       {user === null ? loginForm() : loggedInUser()}
       {user !== null && blogForm()}
       {user !== null &&
-        blogs.map(blog => (
-          <Blog
-            key={blog.id}
-            blog={blog}
-            token={user.token}
-            getAllBlogs={getAllBlogs}
-          />
-        ))}
+        blogs
+          .sort((blog1, blog2) => blog2.likes - blog1.likes)
+          .map(blog => (
+            <Blog
+              key={blog.id}
+              blog={blog}
+              token={user.token}
+              getAllBlogs={getAllBlogs}
+            />
+          ))}
     </div>
   );
 };
