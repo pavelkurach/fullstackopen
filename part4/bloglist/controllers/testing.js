94a4a72cb46bc7299d766c58375bb9970aeeb5ce
Pavel Kurach <pavel.kurach1@gmail.com>
2023-05-19 21:58:00 +0200

    Fix testing router

--- part4/bloglist/controllers/testing.js
+++ part4/bloglist/controllers/testing.js
@@ -1,8 +1,9 @@
 const testingRouter = require('express').Router();
 const Blog = require('../models/blog');
 
-testingRouter.post('/reset/', async () => {
+testingRouter.post('/reset/', async (request, response) => {
   await Blog.deleteMany({});
+  response.status(200).end();
 });
 
 module.exports = testingRouter;
--- part5/cypress/e2e/blog_app.cy.js
+++ part5/cypress/e2e/blog_app.cy.js
@@ -1,5 +1,11 @@
-describe('template spec', () => {
-  it('passes', () => {
+describe('Blog app', function () {
+  beforeEach(function () {
+    cy.request('POST', 'http://localhost:3003/api/testing/reset');
     cy.visit('http://localhost:3000');
+  });
+
+  it('Login form is shown', function () {
+    cy.contains('username:');
+    cy.contains('password');
   });
 });
