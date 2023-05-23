8d8b382a4f7aeda4f190f81c0a4dc1b628f753c2
Pavel Kurach <pavel.kurach1@gmail.com>
2023-05-23 21:44:56 +0200

    Fix .gitignore

--- part6/query-anecdotes/src/App.js
+++ part6/query-anecdotes/src/App.js
@@ -1,6 +1,6 @@
 import AnecdoteForm from './components/AnecdoteForm';
 import Notification from './components/Notification';
-import { useQuery, useMutation, useQueryClient } from 'react-query';
+import { useMutation, useQuery, useQueryClient } from 'react-query';
 import anecdotesService from './services/anecdotes';
 import { useSetNotification } from './NotificationContext';
 
