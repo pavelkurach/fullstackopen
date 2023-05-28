632974cc95d2bd36486e4a6d7ec092c1053b2ab9
Pavel Kurach <pavel.kurach1@gmail.com>
2023-05-28 14:57:56 +0200

    Complete exercise 9.1

---
+++ part9/first-steps/bmiCalculator.ts
@@ -0,0 +1,17 @@
+type kg = number;
+type cm = number;
+
+const calculateBmi = (height: cm, weight: kg): string => {
+  const bmi = weight / Math.pow(height / 100, 2);
+  if (bmi < 18.5) {
+    return 'Underweight';
+  } else if (bmi < 25) {
+    return 'Normal (healthy weight)';
+  } else if (bmi < 30) {
+    return 'Overweight';
+  } else {
+    return 'Obese';
+  }
+};
+
+console.log(calculateBmi(180, 74));
