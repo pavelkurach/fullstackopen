e309b75f5eec74c59ad0608b15db9ab7276d7843
Pavel Kurach <pavel.kurach1@gmail.com>
2023-05-28 17:51:19 +0200

    Complete exercise 9.4

--- part9/first-steps/bmiCalculator.ts
+++ part9/first-steps/bmiCalculator.ts
@@ -4,23 +4,23 @@ type cm = number;
 const calculateBmi = (height: cm, weight: kg): string => {
   const bmi = weight / Math.pow(height / 100, 2);
   if (bmi < 18.5) {
-    return 'Underweight';
+    return "Underweight";
   } else if (bmi < 25) {
-    return 'Normal (healthy weight)';
+    return "Normal (healthy weight)";
   } else if (bmi < 30) {
-    return 'Overweight';
+    return "Overweight";
   } else {
-    return 'Obese';
+    return "Obese";
   }
 };
 
 const parseArguments = (args: Array<string>): [cm, kg] => {
-  if (args.length < 4) throw new Error('Not enough arguments');
-  if (args.length > 4) throw new Error('Too many arguments');
+  if (args.length < 4) throw new Error("Not enough arguments");
+  if (args.length > 4) throw new Error("Too many arguments");
   if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
     return [Number(args[2]), Number(args[3])];
   } else {
-    throw new Error('Provided values were not numbers!');
+    throw new Error("Provided values were not numbers!");
   }
 };
 
@@ -28,5 +28,9 @@ try {
   const [height, weight] = parseArguments(process.argv);
   console.log(calculateBmi(height, weight));
 } catch (e) {
-  console.log('Error, something bad happened, message: ', e.message);
+  let message = "unknwon error";
+  if (e instanceof Error) {
+    message = e.message;
+  }
+  console.log("Error, something bad happened, message: ", message);
 }
--- part9/first-steps/exerciseCalculator.ts
+++ part9/first-steps/exerciseCalculator.ts
@@ -10,7 +10,7 @@ interface Result {
 
 const calculateExercises = (
   dailyExerciseHours: Array<number>,
-  target: number,
+  target: number
 ): Result => {
   const periodLength = dailyExerciseHours.length;
   const trainingDays = dailyExerciseHours.filter((hours) => hours > 0).length;
@@ -18,10 +18,10 @@ const calculateExercises = (
   const success = average >= target;
   const rating = success ? 3 : average >= target - 1 ? 2 : 1;
   const ratingDescription = success
-    ? 'good'
+    ? "good"
     : average >= target - 1
-    ? 'not too bad but could be better'
-    : 'bad';
+    ? "not too bad but could be better"
+    : "bad";
   return {
     periodLength,
     trainingDays,
@@ -34,15 +34,15 @@ const calculateExercises = (
 };
 
 const parseArgumentsExercises = (
-  args: Array<string>,
+  args: Array<string>
 ): [Array<number>, number] => {
-  if (args.length < 4) throw new Error('Not enough arguments');
+  if (args.length < 4) throw new Error("Not enough arguments");
   const target = Number(args[2]);
   const dailyExerciseHours = args.slice(3).map((hours) => Number(hours));
-  if (!isNaN(target) && dailyExerciseHours.every((hours) => !isNaN(hours))) {
+  if (!isNaN(target) && dailyExerciseHours.some((hours) => !isNaN(hours))) {
     return [dailyExerciseHours, target];
   } else {
-    throw new Error('Provided values were not numbers!');
+    throw new Error("Provided values were not numbers!");
   }
 };
 
@@ -50,5 +50,9 @@ try {
   const [dailyExerciseHours, target] = parseArgumentsExercises(process.argv);
   console.log(calculateExercises(dailyExerciseHours, target));
 } catch (e) {
-  console.log('Error, something bad happened, message: ', e.message);
+  let message = "unknwon error";
+  if (e instanceof Error) {
+    message = e.message;
+  }
+  console.log("Error, something bad happened, message: ", message);
 }
