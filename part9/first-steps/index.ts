9126194e0c3e68327bc5d2b82721802293eac350
Pavel Kurach <pavel.kurach1@gmail.com>
2023-05-28 19:47:52 +0200

    Complete exercise 9.7

--- part9/first-steps/index.ts
+++ part9/first-steps/index.ts
@@ -1,22 +1,24 @@
-import express from "express";
-import { calculateBmi } from "./bmiCalculator";
-import { calculateExercises } from "./exerciseCalculator";
+import express from 'express';
+import { calculateBmi } from './bmiCalculator';
+import { calculateExercises } from './exerciseCalculator';
+import bodyParser from 'body-parser';
 
 const app = express();
+app.use(bodyParser.json());
 
-app.get("/hello", (_req, res) => {
-  res.send("Hello, Full Stack!");
+app.get('/hello', (_req, res) => {
+  res.send('Hello, Full Stack!');
 });
 
-app.get("/bmi", (req, res) => {
+app.get('/bmi', (req, res) => {
   const { height, weight } = req.query;
   if (!height || !weight) {
-    res.status(400).json({ error: "missing parameters" });
+    res.status(400).json({ error: 'missing parameters' });
   }
   const heightInCm = Number(height);
   const weightInKg = Number(weight);
   if (isNaN(heightInCm) || isNaN(weightInKg)) {
-    res.status(400).json({ error: "malformatted parameters" });
+    res.status(400).json({ error: 'malformatted parameters' });
   }
   const bmi = calculateBmi(heightInCm, weightInKg);
   res.json({
@@ -26,17 +28,17 @@ app.get("/bmi", (req, res) => {
   });
 });
 
-app.get("/exercises", (req, res) => {
-  const { daily_exercises: dailyExercises, target } = req.query;
+app.post('/exercises', (req, res) => {
+  const { daily_exercises: dailyExercises, target } = req.body;
   if (!dailyExercises || !target) {
-    res.status(400).json({ error: "missing parameters" });
+    res.status(400).json({ error: 'missing parameters' });
   }
-  const dailyExerciseHours = (dailyExercises as string)
-    .split(",")
-    .map((hours) => Number(hours));
+  const dailyExerciseHours = (dailyExercises as number[]).map((hours) =>
+    Number(hours),
+  );
   const targetHours = Number(target);
   if (dailyExerciseHours.some((hours) => isNaN(hours)) || isNaN(targetHours)) {
-    res.status(400).json({ error: "malformatted parameters" });
+    res.status(400).json({ error: 'malformatted parameters' });
   }
   res.json(calculateExercises(dailyExerciseHours, targetHours));
 });
